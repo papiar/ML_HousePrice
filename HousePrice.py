d9cdee3248deffb6fb6f26b06a2613e3e9a179b1
papiar <pablo.pizarro.armendariz@gmail.com>
2019-01-17 23:27:38 +0100

    Clean NA in LotFrontage

--- HousePrice.py
+++ HousePrice.py
@@ -13,14 +13,21 @@ import numpy as py
 
 df = pd.read_csv("test.csv")
 
+#Percentage of null values per column
+limit_na = 0.7
 
-df.shape[1]
-
-#Delete columns with more than 70% of NA
+#Delete columns with more than limit_na of NA
 for col_name in df.columns:
     num_na = df.shape[0] - df[col_name].count()
     #print("For column {} number of NA {}".format(col_name, num_na))
-    if num_na > (df.shape[0] * 0.7):
+    if num_na > (df.shape[0] * limit_na):
         print("Deleted column {}".format(col_name))
         df = df.drop(columns=[col_name])
 
+#Delete id column
+df = df.drop(columns=["Id"])
+
+#Fulfil the na values in the column LotFrontage
+list_Neighborhood = df["Neighborhood"].unique()
+for aux_col in list_Neighborhood:
+    df.loc[(df["LotFrontage"].isna()) & (df["Neighborhood"] == aux_col),"LotFrontage"] = df.loc[(df["Neighborhood"] == aux_col),"LotFrontage"].mean()
